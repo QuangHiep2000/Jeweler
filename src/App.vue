6cc7a0c0badcfcdd88b098fa776aa1b9ba0ccf94
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-03-15 23:13:12 +0700

    Jeweler Page

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <JewelerItem />
+</template>
+
+<script>
+import JewelerItem from './components/JewelerItem.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    JewelerItem,
+    // HelloWorld
+  }
+}
+</script>
+
+
+<style>
+
+</style>
